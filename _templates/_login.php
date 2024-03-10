4b6394280a5b237a414c56c0621545085a5f6b98
Tinu205 <dazleroger404@gmail.com>
2024-03-10 15:41:23 +0530

    Signup page working changed _login page and signup page

--- _templates/_login.php
+++ _templates/_login.php
@@ -1,7 +1,7 @@
 <main class="form-signin w-100 m-auto">
   <form method="post" action="test.php">
     <img class="mb-4" src="/Pictogram/assets/images/pfp.png" alt="" width="72" height="57">
-    <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+    <h1 class="h3 mb-3 fw-normal">Please login</h1>
 
     <div class="form-floating">
       <input name = "username" type="text" class="form-control" id="floatingInputUsername" placeholder="username">
--- _templates/_signup.php
+++ _templates/_signup.php
@@ -1,7 +1,41 @@
-<main class="form-signup w-100 m-auto">
-  <form method="post" action="test.php">
+<?php
+  $signup_stat = false;
+  if(isset($_POST['username']) && isset($_POST['mail']) && isset($_POST['password'])){
+    $no_error = sign_up($_POST['username'],$_POST['mail'],$_POST['password']);
+    $signup_stat = true;
+  }
+?>
+<?php
+  if($signup_stat){
+    if($no_error){
+      ?>
+      <main class="container">
+        <div class="bg-light p-5 rounded mt-3">
+            <h1>Signup Success</h1>
+            <p class="lead">Now you can login from <a href="login.php">here</a>.</p>
+
+        </div>
+      </main>
+      <?php 
+      } else {
+      ?>
+        <main class="container">
+          <div class="bg-light p-5 rounded mt-3">
+              <h1>Signup Fail</h1>
+              <p class="lead">Something went wrong, <?=$error?>
+              </p>
+          </div>
+        </main>
+      <?php
+    } ?>
+<?php } else {
+  print("nothing ")
+  ?>
+
+<main class="form-signin w-100 m-auto">
+  <form method="post" action="signup.php">
     <img class="mb-4" src="/Pictogram/assets/images/pfp.png" alt="" width="72" height="57">
-    <h1 class="h3 mb-3 fw-normal">Please sign up</h1>
+    <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
 
     <div class="form-floating">
       <input name = "username" type="text" class="form-control" id="floatingInputUsername" placeholder="username">
@@ -15,6 +49,16 @@
       <input name = "password" type="password" class="form-control" id="floatingPassword" placeholder="Password">
       <label for="floatingPassword">Password</label>
     </div>
-    <button class="w-100 btn btn-lg btn-primary" type="submit">Sign up</button>
+
+    <div class="checkbox mb-3">
+      <label>
+        <input type="checkbox" value="remember-me"> Remember me
+      </label>
+    </div>
+    <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
   </form>
 </main>
+  
+<?php
+}
+?>
